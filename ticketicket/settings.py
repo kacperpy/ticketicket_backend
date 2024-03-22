8e32d0b9a4c52ccbe568e1cc2b89a30903635101
kacperpy <kacperpy@gmail.com>
2024-03-22 19:19:09 +0100

    celery config

--- ticketicket/settings.py
+++ ticketicket/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Celery settings
-CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
 CELERY_RESULT_BACKEND = 'django-db'
 
 # Celery Beat settings

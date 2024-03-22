0b02414cad428fc6b60fe5559d37cfe6b0a99d5d
kacperpy <kacperpy@gmail.com>
2024-03-22 23:35:38 +0100

    added periodic task serializer & updated search ticket serializer

--- ticket/api/serializers.py
+++ ticket/api/serializers.py
@@ -1,20 +1,44 @@
 from rest_framework import serializers
-
+from django_celery_beat.models import PeriodicTask
 from ticket.models import TicketSearch
 
 
+class PeriodicTaskReadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PeriodicTask
+        fields = [
+            'name',
+            'task'
+        ]
+
+
+class PeriodicTaskCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PeriodicTask
+        fields = [
+            'name',
+            'task',
+            'interval',
+            'expires'
+        ]
+
+
 class TicketSearchCreateSerializer(serializers.ModelSerializer):
+    periodic_task = PeriodicTaskCreateSerializer()
+
     class Meta:
         model = TicketSearch
         fields = [
-            'name',
             'url',
             'price_threshold',
-            'min_availability'
+            'min_availability',
+            'periodic_task'
         ]
 
 
 class TicketSearchReadSerializer(serializers.ModelSerializer):
+    periodic_task = PeriodicTaskReadSerializer(read_only=True)
+
     class Meta:
         model = TicketSearch
         fields = [
@@ -25,5 +49,6 @@ class TicketSearchReadSerializer(serializers.ModelSerializer):
             'name',
             'url',
             'price_threshold',
-            'min_availability'
+            'min_availability',
+            'periodic_task'
         ]

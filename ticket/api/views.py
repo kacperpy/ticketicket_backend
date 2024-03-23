914f7411881465aa0262d08fcbac2a0a4e8434b9
kacperpy <kacperpy@gmail.com>
2024-03-23 18:51:46 +0100

    updated TicketSearch create view; added scheduling tasks functionality

--- ticket/api/views.py
+++ ticket/api/views.py
@@ -1,8 +1,10 @@
+import json
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from ticket.api.serializers import TicketSearchCreateSerializer, TicketSearchReadSerializer
-from ticket.constants import GENERAL_EVENT_NAME
+from ticket.core.constants import GENERAL_EVENT_NAME
 from ticket.models import TicketSearch
+import ticket.tasks.search_ticket as tasks
 
 
 class TicketSearchViewSet(viewsets.ModelViewSet):
@@ -21,7 +23,17 @@ class TicketSearchViewSet(viewsets.ModelViewSet):
         )
         periodic_task = ticket_search.periodic_task
         periodic_task.name = periodic_task_name
+        periodic_task.args = json.dumps(
+            [
+                str(ticket_search.price_threshold),
+                ticket_search.url,
+                ticket_search.min_availability
+            ]
+        )
         periodic_task.save()
+
+        # scheduling a one time task to change the SearchTitle object name
+        tasks.update_search_title.delay(ticket_search.uuid)
 
     def get_serializer_class(self):
         if self.action == 'create':
--- ticket/tasks/search_ticket.py
+++ ticket/tasks/search_ticket.py
@@ -1,18 +1,41 @@
 from celery import shared_task
 from ticket.models import TicketSearch
 from django.contrib.auth import get_user_model
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_website_content(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for 4xx/5xx status codes
+
+        return response.text
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+def extract_title(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+
+    title_text = soup.title.text if soup.title else "Title tag not found"
+
+    return title_text
 
 
 @shared_task
-def test_create_ticket_search(request_user_id):
-    User = get_user_model()
-    created_ticket_search = TicketSearch(
-        created_by=User.objects.get(id=request_user_id),
-        name='test search celery',
-        url='test url',
-        price_threshold=99.99,
-        min_availability=2
-    )
-    created_ticket_search.save()
+def update_search_title(ticket_search_uuid):
+    ticket_search = TicketSearch.objects.get(uuid=ticket_search_uuid)
+    website_content = get_website_content(ticket_search.url)
+    title = extract_title(website_content)
 
-    return f"created ticket search: {created_ticket_search.uuid}"
+    ticket_search.name = title
+    ticket_search.save()
+
+    return f"Updated name to {title}, for Ticket Search {ticket_search_uuid}."
+
+
+@shared_task
+def search_for_ticket(price_threshold, ticket_url, min_availability):
+    return f"{price_threshold} {ticket_url} {min_availability}"
